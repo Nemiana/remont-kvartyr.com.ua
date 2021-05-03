312e6d119cc2bff8c4dcf46c13cb58755538bad7
Nemiana <neminemiana@gmail.com>
2021-05-03 17:48:07 +0300

    captcha

--- config/captcha.php
+++ config/captcha.php
@@ -1,5 +1,17 @@
 ;<?php exit(); ?>
 
 captcha1 = 9;
-captcha2 = 6;
+captcha2 = 8;
 captcha3 = -1;
+captcha4 = -2;
+captcha5 = -6;
+captcha6 = -9;
+captcha7 = 11;
+captcha8 = 0;
+captcha9 = 1;
+captcha10 = 0;
+captcha11 = 1;
+captcha12 = 3;
+captcha13 = 0;
+captcha14 = 1;
+captcha15 = 12;
